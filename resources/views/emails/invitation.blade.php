f9d38831c0a7655f9ad40ddb39e1af1e6faa5ca9
Nilton Romero <niltondeveloper96@gmail.com>
2026-04-11 11:01:34 -0500

    Update Invitation letter

--- resources/views/emails/invitation.blade.php
+++ resources/views/emails/invitation.blade.php
@@ -5,7 +5,7 @@
 </head>
 <body>
     <p>Dear <b>{{ $fullName }}</b>,</p>
-    <p>We are pleased to extend to you an <b>official invitation</b> to attend the <b>18<sup>th</sup> Annual CUGH Conference 2027: Transforming Global Health – Partnerships, Power, Leadership, and Technology in a Rapidly Changing World</b>, taking place in <b>Lima, Peru, from February 25 to 28, 2027</b>.</p>
+    <p>We are pleased to extend to you an <b>official invitation</b> to attend the <b><i>18<sup>th</sup> Annual CUGH Conference 2027: Transforming Global Health – Partnerships, Power, Leadership, and Technology in a Rapidly Changing World</i></b>, taking place in <b>Lima, Peru, from February 25 to 28, 2027</b>.</p>
     <p>Please find your official invitation letter attached, which you may use for visa application or other official purposes.</p>
     <p>We look forward to your participation and are happy to provide any additional information if needed.</p>
     <p>Best regards,<br><b>Organizing Committee</b><br>CUGH 2027<br>+51 983 481 269<br>info@cughlima2027.org</p>
