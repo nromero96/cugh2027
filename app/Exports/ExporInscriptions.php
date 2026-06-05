28b9afda06564d86f665b083ca70a513cfdeaba0
Nilton Romero <niltondeveloper96@gmail.com>
2026-06-05 10:19:12 -0500

    Update export

--- app/Exports/ExporInscriptions.php
+++ app/Exports/ExporInscriptions.php
@@ -20,23 +20,36 @@ class ExporInscriptions implements FromCollection, WithHeadings, WithMapping, Wi
         //join users, payments
         return Inscription::join('users', 'users.id', '=', 'inscriptions.user_id')
                             ->select('inscriptions.id', 
+                                    'users.salutation',
                                     'users.name',
                                     'users.lastname',
                                     'users.second_lastname',
+                                    'users.degrees',
+                                    'users.is_cugh_member',
                                     'users.document_type',
                                     'users.document_number',
-                                    'users.country',
-                                    'users.state',
+                                    'nationalities.name as user_nationality',
+                                    'users.gender',
+                                    'users.occupation',
+
+                                    'users.workplace',
+                                    'users.address',
                                     'users.city',
-                                    'users.address',
-                                    'users.address',
+                                    'users.state',
+                                    'countries.name as user_country',
+
+                                    'users.work_phone_code',
+                                    'users.work_phone_code_city',
+                                    'users.work_phone_number',
                                     'users.phone_code',
                                     'users.phone_number',
                                     'users.whatsapp_code',
                                     'users.whatsapp_number',
                                     'users.email',
-                                    'users.workplace',
+                                    'users.cc_email',
                                     'users.solapin_name',
+                                    'users.solapin_lastname',
+
                                     'category_inscriptions.name as category', 
                                     'inscriptions.special_code',
                                     'inscriptions.price_category',
@@ -46,8 +59,10 @@ class ExporInscriptions implements FromCollection, WithHeadings, WithMapping, Wi
                                     'inscriptions.created_at',
                                     'inscriptions.assistance',
                                     'inscriptions.assistance_accomp')
-                            ->join('category_inscriptions', 'category_inscriptions.id', '=', 'inscriptions.category_inscription_id')
-                            ->where('inscriptions.status', '!=', 'Rechazado')
+                            ->leftjoin('category_inscriptions', 'category_inscriptions.id', '=', 'inscriptions.category_inscription_id')
+                            ->leftjoin('countries as nationalities', 'nationalities.id', '=', 'users.nationality')
+                            ->leftjoin('countries', 'countries.id', '=', 'users.country')
+                            ->where('inscriptions.status', '!=', 'Refused')
                             ->get();
     }
 
@@ -55,30 +70,39 @@ class ExporInscriptions implements FromCollection, WithHeadings, WithMapping, Wi
     {
         return [
             'ID',
-            'Nombre',
-            'Apellido',
-            'Segundo Apellido',
-            'Tipo de documento',
-            'N° Documento',
-            'País',
-            'Estado',
-            'Ciudad',
-            'Dirección',
-            'Dirección',
-            'Teléfono',
-            'Whatsapp',
-            'Email',
-            'Centro de trabajo',
-            'Solapín',
-            'Categoria',
-            'Código Especial',
-            'Precio Categoria',
-            'Pago Total',
-            'Metodo de pago',
-            'Estado',
-            'Fecha de registro',
-            'Asistencia',
-            'Asistencia Acompañante'
+            'Salutation',
+            'First Name',
+            'Middle Name',
+            'Last Name',
+            'Degrees',
+            'CUGH Member',
+            'Document Type',
+            'Document Number',
+            'Nationality',
+            'Gender',
+            'Occupation',
+
+            'Workplace',
+            'Workplace Address',
+            'City',
+            'State',
+            'Country',
+            
+            'Work Phone',
+            'Cell Phone',
+            'WhatsApp',
+            'E-mail',
+            'Cc E-mail',
+            'Solapin Name',
+            'Solapin Last Name',
+
+            'Category',
+            'Price Category',
+            'Special Code',
+            'Total Payment',
+            'Payment Method',
+            'Status',
+            'Registration Date',
         ];
     }
 
@@ -86,39 +110,50 @@ class ExporInscriptions implements FromCollection, WithHeadings, WithMapping, Wi
     {
         return [
             $inscription->id,
+            $inscription->salutation,
             $inscription->name,
             $inscription->lastname,
             $inscription->second_lastname,
+            $inscription->degrees,
+            $inscription->is_cugh_member,
             $inscription->document_type,
             $inscription->document_number,
-            $inscription->country,
-            $inscription->state,
+            $inscription->user_nationality,
+            $inscription->gender,
+            $inscription->occupation,
+
+            $inscription->workplace,
+            $inscription->address,
             $inscription->city,
-            $inscription->address,
-            $inscription->address,
+            $inscription->state,
+            $inscription->user_country,
+
+            $inscription->work_phone_code.' '.$inscription->work_phone_code_city.' '.$inscription->work_phone_number,
             $inscription->phone_code.' '.$inscription->phone_number,
             $inscription->whatsapp_code.' '.$inscription->whatsapp_number,
             $inscription->email,
-            $inscription->workplace,
+            $inscription->cc_email,
             $inscription->solapin_name,
+            $inscription->solapin_lastname,
+
+
             $inscription->category,
-            $inscription->special_code,
             $inscription->price_category,
+            $inscription->special_code,
             $inscription->total,
             $inscription->payment_method,
             $inscription->status,
             $inscription->created_at,
-            $inscription->assistance,
-            $inscription->assistance_accomp
 
         ];
     }
 
     public function styles(Worksheet $sheet){
-        $sheet->getStyle('A1:Z1')->applyFromArray([
+        $sheet->getStyle('A1:AE1')->applyFromArray([
             'font' => [
                 'bold' => true,
                 'color' => ['argb' => 'FFFFFF'],
+                'size' => 12
             ],
             'fill' => [
                 'fillType' => \PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID,
@@ -130,30 +165,36 @@ class ExporInscriptions implements FromCollection, WithHeadings, WithMapping, Wi
         //aplicar anchos de columnas
         $sheet->getColumnDimension('A')->setWidth(3);
         $sheet->getColumnDimension('B')->setWidth(11);
-        $sheet->getColumnDimension('C')->setWidth(11);
-        $sheet->getColumnDimension('D')->setWidth(11);
-        $sheet->getColumnDimension('E')->setWidth(7);
+        $sheet->getColumnDimension('C')->setWidth(22);
+        $sheet->getColumnDimension('D')->setWidth(22);
+        $sheet->getColumnDimension('E')->setWidth(22);
         $sheet->getColumnDimension('F')->setWidth(10);
         $sheet->getColumnDimension('G')->setWidth(7);
         $sheet->getColumnDimension('H')->setWidth(8);
-        $sheet->getColumnDimension('I')->setWidth(11);
-        $sheet->getColumnDimension('J')->setWidth(26);
+        $sheet->getColumnDimension('I')->setWidth(22);
+        $sheet->getColumnDimension('J')->setWidth(14);
         $sheet->getColumnDimension('K')->setWidth(7);
         $sheet->getColumnDimension('L')->setWidth(13);
         $sheet->getColumnDimension('M')->setWidth(13);
         $sheet->getColumnDimension('N')->setWidth(20);
         $sheet->getColumnDimension('O')->setWidth(15);
         $sheet->getColumnDimension('P')->setWidth(18);
-        $sheet->getColumnDimension('Q')->setWidth(26);
-        $sheet->getColumnDimension('R')->setWidth(15);
-        $sheet->getColumnDimension('S')->setWidth(7);
-        $sheet->getColumnDimension('T')->setWidth(7);
-        $sheet->getColumnDimension('U')->setWidth(20);
-        $sheet->getColumnDimension('V')->setWidth(11);
-        $sheet->getColumnDimension('W')->setWidth(17);
-        $sheet->getColumnDimension('X')->setWidth(17);
+        $sheet->getColumnDimension('Q')->setWidth(20);
+        $sheet->getColumnDimension('R')->setWidth(16);
+        $sheet->getColumnDimension('S')->setWidth(16);
+        $sheet->getColumnDimension('T')->setWidth(16);
+        $sheet->getColumnDimension('U')->setWidth(25);
+        $sheet->getColumnDimension('V')->setWidth(20);
+        $sheet->getColumnDimension('W')->setWidth(20);
+        $sheet->getColumnDimension('X')->setWidth(25);
         $sheet->getColumnDimension('Y')->setWidth(22);
-        $sheet->getColumnDimension('Z')->setWidth(22);
+        $sheet->getColumnDimension('Z')->setWidth(14);
+        $sheet->getColumnDimension('AA')->setWidth(17);
+        $sheet->getColumnDimension('AB')->setWidth(14);
+        $sheet->getColumnDimension('AC')->setWidth(22);
+        $sheet->getColumnDimension('AD')->setWidth(22);
+        $sheet->getColumnDimension('AE')->setWidth(22);
+
     }
 
 }
--- app/Http/Controllers/InscriptionController.php
+++ app/Http/Controllers/InscriptionController.php
@@ -164,14 +164,14 @@ class InscriptionController extends Controller
         ];
 
         if (\Auth::user()->hasRole('Administrador') || \Auth::user()->hasRole('Secretaria') || \Auth::user()->hasRole('Hotelero')) {
-            $inscriptions = Inscription::join('category_inscriptions', 'inscriptions.category_inscription_id', '=', 'category_inscriptions.id')
+            $inscriptions = Inscription::leftjoin('category_inscriptions', 'inscriptions.category_inscription_id', '=', 'category_inscriptions.id')
                 ->join('users', 'inscriptions.user_id', '=', 'users.id')
                 ->select('inscriptions.*', 'category_inscriptions.name as category_inscription_name', 'users.name as user_name', 'users.lastname as user_lastname', 'users.second_lastname as user_second_lastname', 'users.country as user_country')
                 ->where('inscriptions.status', 'Refused')
                 ->orderBy('inscriptions.id', 'desc')
                 ->get();
         } else {
-            $inscriptions = Inscription::join('category_inscriptions', 'inscriptions.category_inscription_id', '=', 'category_inscriptions.id')
+            $inscriptions = Inscription::leftjoin('category_inscriptions', 'inscriptions.category_inscription_id', '=', 'category_inscriptions.id')
                 ->join('users', 'inscriptions.user_id', '=', 'users.id')
                 ->select('inscriptions.*', 'category_inscriptions.name as category_inscription_name', 'users.name as user_name', 'users.lastname as user_lastname', 'users.second_lastname as user_second_lastname', 'users.country as user_country')
                 ->where('inscriptions.user_id', $iduser)
