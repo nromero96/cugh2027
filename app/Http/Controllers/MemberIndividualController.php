b43ce1fc0f0a8ec7948fa455e5369de5df7f8eea
Nilton Romero <niltondeveloper96@gmail.com>
2026-06-28 20:10:51 -0500

    Create Member Indivual

---
+++ app/Http/Controllers/MemberIndividualController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\MemberIndividual;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class MemberIndividualController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\MemberIndividual  $memberIndividual
+     * @return \Illuminate\Http\Response
+     */
+    public function show(MemberIndividual $memberIndividual)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\MemberIndividual  $memberIndividual
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(MemberIndividual $memberIndividual)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\MemberIndividual  $memberIndividual
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, MemberIndividual $memberIndividual)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\MemberIndividual  $memberIndividual
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(MemberIndividual $memberIndividual)
+    {
+        //
+    }
+}
---
+++ database/migrations/2026_06_28_200307_create_member_individuals_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMemberIndividualsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('member_individuals', function (Blueprint $table) {
+            $table->id();
+            $table->string('last_name')->nullable();
+            $table->string('first_name')->nullable();
+            $table->boolean('is_active')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('member_individuals');
+    }
+}
